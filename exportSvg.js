2fabcc66478d51ef51cf92aa271e9c6721f4b457
Adam Leis <aleis@iti4dmv.com>
2024-12-17 08:13:56 -0500

    add exportSvg func

---
+++ exportSvg.js
@@ -0,0 +1,18 @@
+export function exportSvg() {
+    const svgXml = '<?xml version="1.0" encoding="UTF-8"?>';
+    const svg = document.body.querySelector('svg');
+    const doc = document.createDocumentFragment();
+    console.log(svg.cloneNode(true));
+    doc.append(svg.cloneNode(true));
+    doc.querySelector('svg').prepend(document.head.querySelector('style').cloneNode(true));
+  
+    const svgStr = new XMLSerializer().serializeToString(doc);
+    const blob = new Blob([svgXml + svgStr], { type: "image/svg+xml" });
+    const url = URL.createObjectURL(blob);
+  
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'graphic.svg';
+    link.click();
+}
+  
